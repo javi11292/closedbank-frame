0bf9fbc53b0f267e84a8e7a6dd83b4370ab49b4f
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-11 03:37:48 +0100

    Add image

---
+++ src/lib/components/image/image.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let src: string;
+</script>
+
+<span class="inline-block overflow-hidden rounded-full">
+	<img class="h-[200px] w-[200px] object-contain" {src} alt="" />
+</span>
