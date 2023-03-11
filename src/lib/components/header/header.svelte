9496aa7d078a09c6c74250a506a0057750eb6e1c
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-11 16:34:24 +0100

    Update onboarding

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -1,3 +1,15 @@
-<header class="flex items-center justify-center bg-neutral-900 py-3 px-3">
+<script lang="ts">
+	import { classes } from "$lib/utils/classes";
+
+	let className: string = "";
+	export { className as class };
+</script>
+
+<header
+	class={classes(
+		className,
+		"flex items-center justify-center bg-neutral-900 py-3 px-3"
+	)}
+>
 	<slot />
 </header>
