9bdf84f5dbf70e3e4a2666d8b4c8d3cfcb673e6c
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-18 12:14:34 +0100

    Test analytics

--- vite.config.ts
+++ vite.config.ts
@@ -34,4 +34,9 @@ export default defineConfig({
 	],
 	server: { port: 3000 },
 	preview: { port: 3000 },
+	define: {
+		"import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(
+			process.env.VERCEL_ANALYTICS_ID
+		),
+	},
 });
